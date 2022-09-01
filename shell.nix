9bd0cc5dcf53e6a6ad7f56783781bb0b5d0a9f14
silky <noonsilk+-noonsilk@gmail.com>
2022-09-01 19:46:37 +0100

    wip on project selection committee page

--- shell.nix
+++ shell.nix
@@ -2,9 +2,11 @@
 
 with pkgs;
 
-let org = builtins.fetchGit {
-            ref = "add-committee-members";
-            url = "https://github.com/FixPlanet/org";
+let org = pkgs.fetchgit {
+            url      = "https://github.com/FixPlanet/org";
+            rev      = "d62c8d9c321f5a2dffa5cac13b384300cf009fcc";
+            sha256   = "sha256-WDUrGrR4DwRSajjSgYPWtdZA9joZUd8hHkWh8/gr0Lc=";
+            fetchLFS = true;
           };
     path = "project-selection-committee";
  in
@@ -12,8 +14,14 @@ let org = builtins.fetchGit {
 mkShell {
   buildInputs = [ ];
   shellHook = ''
+    # Markdown files
     rm -rf src/${path}
     mkdir src/${path}
-    cp ${org.outPath}/${path}/* src/${path}
+    cp ${org.outPath}/${path}/*.md src/${path}
+
+    # Images
+    rm -rf src/images/${path}
+    mkdir src/images/${path}
+    cp ${org.outPath}/${path}/*.{png,jpg} src/images/${path}
   '';
 }
